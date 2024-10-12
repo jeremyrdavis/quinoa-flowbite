aa7ec7dd007874adca7efaa3222dc76b31dd458c
jeremyrdavis <jeremyrdavis@mac.com>
2024-10-12 18:17:32 -0400

    initial

---
+++ src/main/webui/src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+    import "../app.css";
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+<Navbar>
+  <NavBrand href="/">
+    <img src="quarkus-logo-halloween.webp" class="me-3 h-6 sm:h-9" alt="Quarkus Happy AI Halloween" />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Happy AI Halloween</span>
+  </NavBrand>
+  <NavHamburger  />
+  <NavUl >
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/docs/components/navbar">Navbar</NavLi>
+    <NavLi href="/pricing">Pricing</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+  </NavUl>
+</Navbar>
+<div class="grid grid-cols-3 gap-4">
+  <div>
+    <!-- col 1 -->
+    01
+  </div>
+
+  <div>
+    <!-- col 2 -->
+    <slot></slot>
+  </div>
+
+  <div>
+    <!-- col 3 -->
+    03
+  </div>
+</div>
---
+++ src/main/webui/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Alert } from 'flowbite-svelte';
+  import { Input, Label, Helper, Button } from 'flowbite-svelte';
+</script>
+
+<h1>Welcome to Red Hat Halloween AI Assistant</h1>
+<form>
+  <div class="mb-6">
+    <div>
+      <Label for="player_name" class="mb-2">Your name</Label>
+      <Input type="text" id="player_name" placeholder="Ichibod Crane" required />
+    </div>
+    </div>
+    <Button type="submit">Submit</Button>
+</form>
